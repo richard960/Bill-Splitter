e5a96b1cb4e9211aa036b1841282b5f27edbf0de
Richard Medina <medinarichard996@gmail.com>
2021-07-26 11:01:18 -0700

    server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path')
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen('3000', () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('you are connected')
 })
